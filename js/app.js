815b932285207d31c1ec0b015e8497b4f28a5655
Ulises Baroni <ulisesbaroni@gmail.com>
2022-08-30 13:02:46 -0300

    Proyecto JS BURGBEER

---
+++ js/app.js
@@ -0,0 +1,176 @@
+
+
+// MESAS/UBICACIÓN
+const mesas = [
+
+    {
+        mesa: 10,
+        code: 'teclado'
+    },
+    {
+        mesa: 15,
+        code: 'mouse'
+    },
+    {
+        mesa: 20,
+        code: 'monitor'
+    },
+
+]
+
+
+const mesaLogin = document.getElementById('numeroMesa'),
+    codigoLogin = document.getElementById('codigoMesa'),
+    recordar = document.getElementById('recordar'),
+    btnLogin = document.getElementById('login'),
+    modalEl = document.getElementById('modalLogin'),
+    modal = new bootstrap.Modal(modalEl),
+    view = document.querySelectorAll('.view');
+
+
+
+//Guardar datos
+function guardarDatos(mesaRegistrada, storage) {
+
+    const ubicacion = {
+        'number': mesaRegistrada.mesa,
+        'code': mesaRegistrada.code
+    }
+
+    storage.setItem('ubicacion', JSON.stringify(ubicacion));
+}
+
+//Recuperar datos guardados para poder ingresar
+function recuperarRegistro(storage) {
+    return JSON.parse(storage.getItem('ubicacion'));
+}
+
+
+function saludar(ubicacion) {
+    saludo.innerHTML = `Mesa <span>#</span>  ${ubicacion.number}`
+}
+
+//Intercambiar clases para la visualizacion de algunos elementos.
+function intercambiarClases(elemento, clase) {
+    elemento.forEach(element => {
+        element.classList.toggle(clase)
+    })
+}
+
+//Validar mesa con la informacion que ingresa el usuario
+function validarMesa(BDmesas, number, code) {
+    let encontrada = BDmesas.find((BDmesa) => BDmesa.mesa == number);
+
+    if (typeof encontrada === 'undefined') {
+        return false;
+
+    } else {
+
+        if (encontrada.code != code) {
+            return false;
+        } else {
+            return encontrada;
+        }
+    }
+}
+
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //Validar que ambos campos estén completos
+    if (!mesaLogin.value || !codigoLogin.value) {
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Lo lamento...',
+            text: 'Todos los campos son obligatorios para poder iniciar tu pedido!',
+            html: 'Todos los campos son obligatorios para poder iniciar tu pedido!'
+        });
+
+    } else {
+        let data = validarMesa(mesas, mesaLogin.value, codigoLogin.value);
+
+        //Revisar si el return de la funcion validar es objeto o booleano.Si es objeto, se valido correctamente! Si no, informar por alert!
+
+        if (!data) {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Número de mesa y/o código incorrecto',
+                text: 'Intenta nuevamente por favor!'
+            });
+
+            //Checkear si el cliente elige o no recordar los datos, si elige recordarlos, guardarlos en el localStorage, si no, en el sessionStorage
+        } else {
+
+            if (recordar.checked) {
+                guardarDatos(data, localStorage);
+                saludar(recuperarRegistro(localStorage));
+
+
+            } else {
+                guardarDatos(data, sessionStorage);
+                saludar(recuperarRegistro(sessionStorage));
+
+            }
+
+            modal.hide();
+
+            intercambiarClases(view, 'd-none');
+
+        }
+    }
+})
+
+
+//Revisar y evitar validar mesa si esta guardada en el storage
+function checkRegistro(ubicacion) {
+
+    if (ubicacion) {
+        saludar(ubicacion);
+        intercambiarClases(view, 'd-none');
+    }
+}
+
+checkRegistro(recuperarRegistro(localStorage));
+
+
+// Traer productos del .json
+const fetchData = async () => {
+    const res = await fetch('./js/api.json');
+    return await res.json()
+}
+
+let productos
+fetchData().then(resp => {
+    productos = resp
+    mostrarProductos(productos)
+})
+
+
+// Contenedor de prodcutos
+const mostrarProductos = (productos) => {
+    const contenedorProductos = document.getElementById('contenedor')
+    productos.forEach(producto => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `<div class="card" style="width: 18rem;">
+                            <img src="${producto.img}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p class="card-text">Info:  ${producto.ingredientes}</p>
+                                <p class="card-precio">Precio: $${producto.precio}</p>
+                                <div class='btn-añadir'><button class="btn btn-success" id=boton${producto.id}>Sumar al carrito</button></div>
+                                </div>
+                            </div>`
+
+        contenedorProductos.appendChild(div)
+
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener('click', () => {
+            carritoIndex(producto.id)
+        })
+
+    })
+
+}
---
+++ js/modal-carrito.js
@@ -0,0 +1,154 @@
+
+// CARRITO DE COMPRA
+const carritoDeCompras = []
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+
+// Modal carrito de compra
+const carritoIndex = (productoId) => {
+
+    const renderProdcutoCarrito = () => {
+        let producto = productos.find(producto => producto.id == productoId);
+
+
+        //Si existe producto en carrito, sumarle una unidad
+        const existe = carritoDeCompras.find(x => x.id === producto.id);
+
+        if (existe) {
+            carritoDeCompras.forEach(x => x.id === existe.id ? x.cantidad++ : x.cantidad);
+        } else {
+            carritoDeCompras.push(producto);
+        }
+
+        contenedorCarrito.innerHTML = '';
+        carritoDeCompras.forEach(element => {
+
+            let div = document.createElement('div')
+            div.classList.add('productoEnCarrito')
+            div.innerHTML = `<p class= 'title'>${element.nombre}</p>
+                        <p>Precio: $${element.precio}</p> 
+                        <p id="cantidad${element.id}">Cantidad: ${element.cantidad}</p>
+                        <button id="eliminar${element.id}" class="delete btn btn-danger btn-small boton-eliminar" ><i class=" fa fa-trash"></i></button>
+                        `;
+
+            contenedorCarrito.appendChild(div)
+
+            div.querySelector('.delete').addEventListener('click', removeItemCarrito)
+
+            carritoTotal()
+        });
+    }
+
+    renderProdcutoCarrito()
+
+
+}
+
+//Calcular Total consumo
+function carritoTotal() {
+    let total = 0;
+    const itemCarTotal = document.getElementsByClassName('itemCarTotal');
+    carritoDeCompras.forEach(element => {
+        total = total + element.precio * element.cantidad
+    });
+
+    const elementoHTML = [...itemCarTotal];
+    elementoHTML.forEach(elemento => {
+        elemento.innerHTML = `Total: $ ${total}`
+    })
+
+}
+
+
+//Quitar productos del carrito
+function removeItemCarrito(e) {
+    const buttonDelete = e.target
+    const div = buttonDelete.closest('.productoEnCarrito')
+
+
+    //Hacer que al remover producto del carrito, se descuente del monto total
+    const title = div.querySelector('.title').textContent;
+    for (let i = 0; i < carritoDeCompras.length; i++) {
+        if (carritoDeCompras[i].nombre.trim() === title.trim()) {
+            carritoDeCompras[i].cantidad = 1;
+            carritoDeCompras.splice(i, 1)
+        }
+    }
+
+    div.remove()
+    carritoTotal()
+    
+}
+
+
+//Enviar pedido
+const enviarPedido = document.querySelector('.send')
+enviarPedido.addEventListener('click', () => {
+
+        if (carritoDeCompras.length !== 0) {
+
+            Swal.fire({
+                
+                title: '¡Envío exitoso!',
+                imageUrl: '/img/ok.jpg',
+                html: '<b>Tu pedido esta siendo preparado!</b> <br><br>' +
+                    '<b>Tu opinión nos hace crecer!</b> ' +
+                    'Por eso nos gustaría saber.. ¿Que te parece nuestro sistema de pedido digital? ',
+                showCloseButton: false,
+                allowOutsideClick: false,
+                showCancelButton: true,
+                focusConfirm: false,
+                confirmButtonText: '<i class="fa fa-thumbs-up"></i> Genial!',
+                confirmButtonAriaLabel: 'Thumbs up, great!',
+                cancelButtonText: '<i class="fa fa-thumbs-down"></i> Aún lo pueden mejorar!',
+
+
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        position: 'center',
+                        imageUrl: '/img/si.jpg',
+                        title: '¡Buenísimol! Nos alegra muchísimo saber eso. Muchas gracias por tu opinión!',
+                        showConfirmButton: false,
+                        timer: 3500
+                    })
+
+                } else {
+                    Swal.fire({
+                        position: 'center',
+                        imageUrl: '/img/no.jpg',
+                        title: 'Excelente, pensaremos nuevas ideas para mejorar nuestro servicio! Muchas gracias por tu opinión!',
+                        showConfirmButton: false,
+                        timer: 4000,
+
+                    })
+
+                }
+
+            })
+
+
+        } else {
+            Swal.fire({
+                position: 'center',
+                imageUrl: '/img/error.jpg',
+                title: '¡Ay carammmba!',
+                text: 'Parece que no hay productos en tu carrito, por favor selecciona un producto para poder enviar el pedido!',
+
+            })
+
+        }
+
+        //Vaciar carrito una vez finalizada la compra
+        while (carritoDeCompras.length !== 0) {
+            carritoDeCompras[carritoDeCompras.length -1 ].cantidad = 1;
+            carritoDeCompras.pop()
+            contenedorCarrito.innerHTML = '';
+            carritoTotal()
+            
+        }
+       
+
+    }
+
+)
